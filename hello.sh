18159ff44fe9c34f59d8b80ef86fd380774b9fa4
SriraagSridhar <sripri.24@gmail.com>
2018-12-31 20:41:31 +0530

    Changed the shell script and added a test case statement

--- hello.sh
+++ hello.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "Hello World"
+while (true)
+do
+echo "Hello, how are you doing today"
+read input
+	case $input in
+		good ) 	echo "Good"
+			;;
+		bad )	echo "Dont Worry this shall pass too!"
+			;;
+		q )	echo "Quitting now !!";exit
+			;;
+		* )	echo "\nUnknown Input try good or bad"
+			;;
+	esac
 
+done
 #End of the shell script
